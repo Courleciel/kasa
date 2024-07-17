9c96b68175b93f1b6137348dab2d2c501869d251
luc perrot <luc.perrot@hotmail.fr>
2024-07-17 14:57:51 +0200

    card component created + sticky footer fixed

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,13 @@
 import Banner from '../../components/Banner';
+import Card from '../../components/Card';
 
 function Home() {
   return (
     <div>
       <Banner />
+      <div className='cards'>
+        <Card />
+      </div>
     </div>
   )
 }
