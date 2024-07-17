fc39f253933857b62c93940c6056491559fc7e36
luc perrot <luc.perrot@hotmail.fr>
2024-07-17 11:46:53 +0200

    first styles with SASS

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './assets/styles/main.scss'
 import Header from './components/Header';
 import Home from './pages/Home';
 import Housing from './pages/Housing';
