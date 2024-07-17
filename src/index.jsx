6acfb8cc28a6c8544b5c09127a517c4b5e2af5f6
luc perrot <luc.perrot@hotmail.fr>
2024-07-17 11:07:56 +0200

    setup of Router + change of project architectures

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+ReactDOM.render(
+  <React.StrictMode>
+      <Router>
+          <Routes>
+          </Routes>
+                </Router>
+  </React.StrictMode>,
+document.getElementById('root')
+)
