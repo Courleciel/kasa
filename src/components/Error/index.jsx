c2a3c36391e1c838921d875bdea254053c12cde0
luc perrot <luc.perrot@hotmail.fr>
2024-07-19 11:15:57 +0200

    Error page done + definition of primary color variable

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function Error() {
   return (
-      <div>
-          <h1>Oups la page que vous demandez n'existe pas</h1>
-      </div>
-  )
+    <div className="error-container">
+      <h1 className="error-title">404</h1>
+      <p className="error-message">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="error-link">Revenir à la page d'accueil</Link>
+    </div>
+  );
 }
 
-export default Error
+export default Error;
